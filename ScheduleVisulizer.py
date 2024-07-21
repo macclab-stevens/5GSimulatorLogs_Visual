ce809b6ee28713311a6d1ac15175e58dab255022
Eric-Forbes <163568786+Eric-Forbes@users.noreply.github.com>
2024-07-21 15:08:40 -0400

    readFile
    Cleaned up datastructure import for pandas

--- ScheduleVisulizer.py
+++ ScheduleVisulizer.py
@@ -5,8 +5,10 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 import scipy.io
 import pandas as pd
+import numpy as np
 
 def readFile(fileName):
+    print("readFile()")
     mat = scipy.io.loadmat(fileName)
     
     #pull the relevant arrary from
@@ -15,20 +17,49 @@ def readFile(fileName):
     
     #convert to DataFrame
     df = pd.DataFrame(simLogs)
-    removals  = '|'.join(['[', ']'])
-    for i in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]:
+    
+    #Remove array chars
+    for i in range(15):
         df[i] = df[i].astype(str).str.replace('[','')
         df[i] = df[i].astype(str).str.replace(']','')
         df[i] = df[i].astype(str).str.replace('\'','')
+        df[i] = df[i].astype(str).str.replace(';',' ')
+    
+    #make new DF with the right column
     header = df.iloc[0]
     df  = pd.DataFrame(df.values[1:], columns=header)
-    # df["rnti"] = pd.to_numeric(df["rnti"])
+    # print(header)
+    df = df.rename(columns={'RBG Allocation Map':'RBG',
+                            'Feedback Slot Offset (DL grants only)':'FdbkOffst',
+                            'CQI on RBs':'CQIs'})
+    
+    #convert relevant columns to the right types
+    toInts = ['RNTI','Frame','Slot','Start Sym', 'Num Sym', 'MCS', 'NumLayers', 'HARQ ID', 'NDI Flag', 'RV']
+    #--INTS
+    for i in toInts:
+        df[i] = pd.to_numeric(df[i])
+    toStrs = ['Grant type','Tx Type']
+    #--Strings
+    for i in toStrs:
+        df[i] = df[i].astype(str)
+    #--ARRAYS
+    df['RBG'] = df['RBG'].astype(str).str.split(pat=' ')
+    df['RBG'] = df['RBG'].apply(lambda lst: list(map(int, lst)))
+    df['CQIs'] = df['CQIs'].astype(str).str.split(pat=' ')
+    df['CQIs'] = df['CQIs'].apply(lambda lst: list(map(int, lst)))
+
     return df
+
+def mergeRBG(df):
+    print('mergeRBG()')
+    allos = pd.DataFrame()
+    return allos
 
 def main():
     print("Main()")
     df = readFile('simulationLogs.mat')
-    print(df)
+    mergeRBG(df)
+
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
